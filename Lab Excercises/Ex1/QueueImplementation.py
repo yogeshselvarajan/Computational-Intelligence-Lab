c1336ec9d3a2fdf01f2440eb30010accf45eb9a0
Yogesh S <yogeshselvarajan@gmail.com>
2023-07-13 00:27:58 +0530

    Changed Directory Structure

---
+++ Lab Excercises/Ex1/QueueImplementation.py
@@ -0,0 +1,16 @@
+class Queue:
+
+    def __init__(self):
+        self.items = []
+
+    def enqueue(self, item):
+        self.items.append(item)
+
+    def dequeue(self):
+        return self.items.pop(0)
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def __len__(self):
+        return len(self.items)
